e71125d786753c4c7d541068ece01eae90676eb1
Juan Cruz <juancasserab@gmail.com>
2025-04-27 20:44:53 -0300

    Arreglo ejercicio 1.

--- Práctica/3/Ejercicio 1/paralelo.c
+++ Práctica/3/Ejercicio 1/paralelo.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<omp.h>
+#include<sys/time.h>
+
+double dwalltime() {
+    double sec;
+    struct timeval tv;
+    gettimeofday( & tv, NULL);
+    sec = tv.tv_sec + tv.tv_usec / 1000000.0;
+    return sec;
+}
 
 int main(int argc, char * argv[]) {
+    double timetick;
     double * A;
     int i, j;
     int check = 1;
@@ -17,12 +27,16 @@ int main(int argc, char * argv[]) {
     // Inicializa la matriz. Cada posicion debe quedar con el valor I*J
     // I => fila J=> columna.
 
+    timetick = dwalltime();
+
     #pragma omp parallel for shared(A) private(i, j)
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             A[i * N + j] = i * j;
         }
     }
+
+    printf("Tiempo de ejecución: %f segundos\n", dwalltime() - timetick);
 
     // Verifica el resultado
     for (i = 0; i < N; i++) {
