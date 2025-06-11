ce87e3f14deb18c9324ca021dbbaf0f81d8b7753
Juan Cruz <juancasserab@gmail.com>
2025-06-11 14:01:00 -0300

    Arreglo el .sh hibrido

--- TPs/3/Entrega/script-hibrido.sh
+++ TPs/3/Entrega/script-hibrido.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -N 2
 #SBATCH --exclusive
-#SBATCH --tasks-per-node=8
-#SBATCH -o hibrido_resultados/output_N_?_T_?.txt
-#SBATCH -e hibrido_resultados/errors_N_?_T_?.txt
-export OMP_NUM_THREADS=4
+#SBATCH --tasks-per-node=1
+#SBATCH -o hibrido_resultados/output_N_?.txt
+#SBATCH -e hibrido_resultados/errors_N_?.txt
+export OMP_NUM_THREADS=8
 mpirun --bind-to none hibrido $1
